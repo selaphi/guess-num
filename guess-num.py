135d2ee94a41d3a19e830a716312201bea973d7d
Selaphi <selaphi@gmail.com>
2021-08-31 22:44:33 +0800

    new edition

--- guess-num.py
+++ guess-num.py
@@ -2,9 +2,9 @@ import random
 x = input('這是個猜數字遊戲，請決定最大的數字範圍是多少: ')
 x = int(x)
 r = random.randint(1, x)
-i = 0
+count = 0
 while True:
-	i = i + 1
+	count = count + 1
 	guess = input('請猜1個數字:')
 	guess = int(guess)
 	if guess == r:
@@ -14,5 +14,5 @@ while True:
 		print('要再小一點')
 	elif guess < r:	
 		print('要再大一點')
-	if i > 0:
-		print('你已經猜了', i, '次')
+	print('你已經猜了', count, '次')
+		
