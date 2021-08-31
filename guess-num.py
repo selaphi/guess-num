3ba62696218d12d5797a98d9272fc3ec1719c10a
Selaphi <selaphi@gmail.com>
2021-08-31 22:35:31 +0800

    self decided range

--- guess-num.py
+++ guess-num.py
@@ -1,7 +1,11 @@
 import random
-r = random.randint(1, 100)
+x = input('這是個猜數字遊戲，請決定最大的數字範圍是多少: ')
+x = int(x)
+r = random.randint(1, x)
+i = 0
 while True:
-	guess = input('請猜1到100中的數字:')
+	i = i + 1
+	guess = input('請猜1個數字:')
 	guess = int(guess)
 	if guess == r:
 		print('恭喜你，猜對了!')
@@ -10,3 +14,5 @@ while True:
 		print('要再小一點')
 	elif guess < r:	
 		print('要再大一點')
+	if i > 0:
+		print('你已經猜了', i, '次')
